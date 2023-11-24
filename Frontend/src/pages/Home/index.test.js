e7e89c0d80eb2bb44952f3a647423eedc503d9bf
SolineMP <56126066+SolineMP@users.noreply.github.com>
2023-11-24 16:24:25 +0100

    Correctif dossiers

--- Frontend/src/pages/Home/index.test.js
+++ Frontend/src/pages/Home/index.test.js
@@ -1,13 +1,13 @@
 import { MemoryRouter } from 'react-router-dom'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Card from '../../components/Card'
 import Banner from '../../components/Banner'
 
  
 describe('Header, Card and Footer', () => {
-    test('Should render without crash', async () => {
+    it('Should render without crash', async () => {
         render(
             <MemoryRouter>
                 <Header />
@@ -18,3 +18,4 @@ describe('Header, Card and Footer', () => {
         )
     })
 })
+

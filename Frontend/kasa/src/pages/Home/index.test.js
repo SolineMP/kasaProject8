0bbceee622302acbcfa0a7d6a87fb21472eb7217
SolineMP <56126066+SolineMP@users.noreply.github.com>
2023-11-24 15:57:29 +0100

    complete home test

--- Frontend/kasa/src/pages/Home/index.test.js
+++ Frontend/kasa/src/pages/Home/index.test.js
@@ -1,12 +1,18 @@
 import { MemoryRouter } from 'react-router-dom'
-import Footer from './'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 import { render } from '@testing-library/react'
+import Card from '../../components/Card'
+import Banner from '../../components/Banner'
 
  
-describe('Footer', () => {
+describe('Header, Card and Footer', () => {
     test('Should render without crash', async () => {
         render(
             <MemoryRouter>
+                <Header />
+                <Banner />
+                <Card />
                 <Footer />
             </MemoryRouter>
         )
